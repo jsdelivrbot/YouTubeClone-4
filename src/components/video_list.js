b6f05d52ef0d4b7288d6d7fabf39d091a6e535fa
Amin <amin7x6@gmail.com>
2018-04-11 22:25:14 -0700

    add styling

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -5,14 +5,14 @@ const VideoList = (props) => {
   const videoItems = props.videos.map((video) => {
     return (
       <VideoListItem
-        onVideoSelect={props.onVideoSelect} 
+        onVideoSelect={props.onVideoSelect}
         key={video.etag}
         video={video} />
     )
   });
 
   return (
-    <ul className="col-md-8 list-group">
+    <ul className="col-md-4 list-group">
       {videoItems}
     </ul>
   )
