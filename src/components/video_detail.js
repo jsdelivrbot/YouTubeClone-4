a4dfb36a926d88ba6b5ba42e2d74a9efe61df21a
Amin <amin7x6@gmail.com>
2018-04-11 21:11:06 -0700

    add video List Item

---
+++ src/components/video_detail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VideoDetail  = (props) => {
+  <div className="video-details col-md-8">
+    <div className="embed-responsive embed-rsponsive-16by9">
+      <iframe classname="embed-responsive-item"></iframe>
+    </div>
+  </div>
+};
+
+export default VideoDetail;
